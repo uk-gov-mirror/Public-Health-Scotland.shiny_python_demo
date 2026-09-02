07d2a2c71818dfbf7cd2775a5ea968bc6e80a7cb
gafo25 <gsandrof66@gmail.com>
2026-09-02 22:53:53 +0100

    checked duckdb encrypted

--- tools/csv_to_encrypted_duckdb.py
+++ tools/csv_to_encrypted_duckdb.py
@@ -9,19 +9,19 @@ csv_path = "data/WHR2024.csv"
 db_path = "data/encrypted_data.duckdb"
 encryption_key = str(os.getenv('PWD'))
 
-# # Create an encrypted database
-# con = duckdb.connect(db_path, config={"password": encryption_key})
+# Create an encrypted database
+con = duckdb.connect(db_path, config={"password": encryption_key})
 
-# # 2. Import the CSV file into an encrypted table within the attached database
-# con.execute(f"""
-#     CREATE TABLE my_table AS 
-#     SELECT * FROM read_csv_auto('{csv_path}');
-# """)
+# 2. Import the CSV file into an encrypted table within the attached database
+con.execute(f"""
+    CREATE TABLE my_table AS 
+    SELECT * FROM read_csv_auto('{csv_path}');
+""")
 
-# print("Successfully created encrypted DuckDB file and imported CSV data.")
+print("Successfully created encrypted DuckDB file and imported CSV data.")
 
-# # Close the connection
-# con.close()
+# Close the connection
+con.close()
 
 # test data
 con = duckdb.connect(db_path, read_only=True, config={"password": encryption_key})
